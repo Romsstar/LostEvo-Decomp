77f13d3c58782a4acfbcbb90be5b349eed3f67aa
Romsstar <romsstar@gmail.com>
2022-11-12 12:40:58 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace LostEvoRewrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.extractToolStripMenuItem,
+            this.importToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // extractToolStripMenuItem
+            // 
+            this.extractToolStripMenuItem.Name = "extractToolStripMenuItem";
+            this.extractToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.extractToolStripMenuItem.Text = "Extract";
+            this.extractToolStripMenuItem.Click += new System.EventHandler(this.extractToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.importToolStripMenuItem.Text = "Import";
+            this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem extractToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LostEvoRewrite
+{
+
+    public struct partition
+    {
+        public string filenum;
+        public uint offset;
+        public uint decsize;
+        public uint encsize;
+        public uint flag; //0x80000000 = uncompressed, 0x0 = compressed
+    };
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public byte[] data;
+
+        public BinaryReader br;
+        public uint numPartitions;
+        public List<partition> partitionList = new List<partition>();
+        public static string dir = "extracted//";
+        private static int[] text_buf = new int[4113];
+        private static int match_position, match_length, lson, rson, dad;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            {
+                openFileDialog1.Filter = "PAK files|*.PAK|All Files|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string PAKFile = openFileDialog1.FileName;
+                    FileStream fs = new FileStream(PAKFile, FileMode.Open);
+                    br = new BinaryReader(fs);
+                    numPartitions = br.ReadUInt32();
+                    char[] sVersion = br.ReadChars(12);
+
+
+                    partition partitionArray = new partition();
+                    for (int i = 0; i < numPartitions; i++)
+                    {
+                        partitionArray.filenum = "" + i;
+                        partitionArray.offset = br.ReadUInt32();
+                        partitionArray.decsize = br.ReadUInt32();
+                        partitionArray.encsize = br.ReadUInt32();
+                        partitionArray.flag = br.ReadUInt32();
+                        partitionList.Add(partitionArray);
+                    }
+
+                }
+            }
+        }
+
+        int PaddingCalc(int pos, int align)
+        {
+            int tmp = (align - 1);
+            align -= (pos & tmp);
+            return tmp & align;
+        } 
+
+        private void extractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            foreach (var entry in partitionList)
+            {
+                br.BaseStream.Seek(entry.offset, SeekOrigin.Begin);
+          
+                    data = br.ReadBytes((int)entry.encsize);
+
+                File.WriteAllBytes(dir + (entry.filenum) + ".bin", data);
+            }
+        }
+
+        private byte paddingCalc(int pos, byte align)
+        {
+            byte tmp = (byte)(align - 1);
+            align -= (byte)(pos & tmp);
+            return (byte)(tmp & align);
+        }
+
+        private byte[] Decompress(byte[] src)
+        {
+            var buffer = new List<byte>();
+            byte[] buffer2 = new byte[4096];
+            uint r0, curResult, r2, r3, flagByte, curBuff, r6, buferIdx, r9, curResult2, lr;
+            r0 = 0;
+            curResult = 0;
+            r2 = 0;
+
+            curBuff = 4078;
+            r6 = r2;
+            buferIdx = r0;
+            flagByte = 0;
+     
+
+            Array.Clear(buffer2, (int)flagByte, (int)curBuff);
+
+            r0 = BitConverter.ToUInt32(src, (int)buferIdx);
+            buferIdx += 4;
+
+            
+            while (r0 > 0)
+            {
+          
+                curResult = flagByte << 15;
+                flagByte = curResult >> 16;
+
+         
+                if ((flagByte & 0x100) != 0x100)
+                {
+                    r0 = r0 - 1;
+                    curResult = src[buferIdx++];
+                    if ((int)r0 < 0) break;
+
+                    curResult = curResult | 0xFF00;
+                    curResult = curResult << 16;
+                    flagByte = curResult >> 16;
+                }
+
+               
+                if ((flagByte & 0x1) == 0x1)
+                {
+                    
+                    r0 = r0 - 1;
+                    curResult = src[buferIdx++];
+                    if ((int)r0 < 0) break;
+                    r3 = curResult & 0xFF;
+                    buffer.Add((byte)r3);
+                    curResult = 4095;
+                    r2 = curBuff + 1;
+                    buffer2[r6 + curBuff] = (byte)r3;
+                    curBuff = r2 & curResult;
+                    continue;
+                }
+
+                r3 = src[buferIdx];
+                curResult = r0 - 1;
+                if ((int)curResult < 0) break;
+                curResult = src[buferIdx + 1];
+                buferIdx = buferIdx + 2;
+                r0 = r0 - 2;
+                if ((int)r0 < 0) break;
+                r2 = curResult & 0xF0;
+                curResult = curResult & 0xF;
+                r9 = curResult + 2;
+                curResult = 4095;
+                curResult2 = r3 | (r2 << 4);
+                lr = 0;
+
+                do
+                {
+         
+                    r2 = curResult2 + lr;
+                    r2 = r2 & curResult;
+                    r3 = buffer2[r6 + r2];
+                    lr = lr + 1;
+                    r2 = curBuff + 1;         
+                    buffer.Add((byte)r3);
+                    buffer2[r6 + curBuff] = (byte)r3;
+                    curBuff = r2 & curResult;
+                } while (lr <= r9);
+            }
+
+
+            return buffer.ToArray();
+        }
+
+
+
+
+
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    var path = fbd.SelectedPath;
+                    var inputFiles = Directory.EnumerateFiles(path).ToArray();
+                    long padding = 0;
+                    byte[] pad;
+                    var i = 0;
+                    int offset = inputFiles.Length * 16 + 16;
+                    using (var output = File.Open("table.bin", FileMode.Create))
+                    using (var bw = new BinaryWriter(output, Encoding.UTF8, false))
+                    {
+                        bw.Write(inputFiles.Length);
+                        bw.Write(0x31302e32);
+                        bw.Write(padding);
+
+
+                        using (var stream = File.OpenWrite("result.bin"))
+                        {
+                            foreach (var inputFile in inputFiles)
+                            {
+                                var currentBytes = File.ReadAllBytes(inputFile);
+                                //bw.Write(offset);
+                                //if (partitionList[i].flag == 0)
+                                //{
+
+                                //    bw.Write(Decompress(currentBytes).Length);
+
+                                //}
+                                //else
+                                //{
+                                //    bw.Write(currentBytes.Length);
+
+                                //}
+                                //bw.Write(currentBytes.Length);
+                                //bw.Write(partitionList[i].flag);
+
+                                //offset += currentBytes.Length;
+                                //i++;
+                                stream.Write(currentBytes, 0, currentBytes.Length);
+                                stream.Write(new byte[0], 0, paddingCalc((int)stream.Position, 0x20));
+                            }
+
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
